594e04657ff2d63a7dfc510ca7ddb74b8182dc91
rock huang <manj22@163.com>
2025-10-18 17:12:32 +0800

    fix: open file dialog failed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,10 +23,7 @@ export default defineNuxtConfig({
     build: {
       target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
       minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-      sourcemap: !!process.env.TAURI_DEBUG,
-      rollupOptions: {
-        external: ['@tauri-apps/plugin-dialog']
-      }
+      sourcemap: !!process.env.TAURI_DEBUG
     }
   }
 })
